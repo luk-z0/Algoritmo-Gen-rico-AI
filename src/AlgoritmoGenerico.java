4256e77f248b55b14c81487c72853bd32c3c8b90
Lucas Gabriel <70025847+luk-z0@users.noreply.github.com>
2022-10-25 07:58:38 -0300

    Add files via upload

---
+++ src/AlgoritmoGenerico.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class AlgoritmoGenerico {
+
+    public double random() {
+
+        double MIN = -100000;
+        double MAX = 100000;
+        return (MAX - MIN) + MIN;
+    }
+
+    public double funcao(double x1, double x2) {
+        return (x1 - Math.log(x2)) / (Math.pow(x1, 2) - 3 * x2);
+    }
+
+    public ArrayList<Individuo> populacao(int n) {
+        ArrayList<Individuo> popupalcao = new ArrayList<>();
+        Random r = new Random();
+        for (int i = 0; i < n; i++) {
+            double x1 = r.nextDouble(this.random());
+            double x2 = r.nextDouble(this.random());
+            double fitness = this.funcao(x1, x2);
+            popupalcao.add(new Individuo(x1, x2, fitness));
+        }
+        return popupalcao;
+    }
+
+    public Individuo selecao(ArrayList<Individuo> populacao) {
+        Random r = new Random();
+        int index1 = r.nextInt(populacao.size() - 1);
+        int index2 = r.nextInt(populacao.size() - 1);
+        // int index3 = r.nextInt(populacao.size() - 1);
+        Individuo individuo1 = populacao.get(index1);
+        Individuo individuo2 = populacao.get(index2);
+        // Individuo individuo3 = populacao.get(index3);
+
+        if (individuo1.getFit() > individuo2.getFit())
+            return individuo1;
+        return individuo2;
+
+    }
+
+    public ArrayList<Individuo> cruzamento(Individuo pai1, Individuo pai2) {
+        ArrayList<Individuo> filhos = new ArrayList<>();
+
+        double x1Pai1 = pai1.getX1();
+        double x2Pai1 = pai1.getX2();
+
+        double x1Pai2 = pai2.getX1();
+        double x2Pai2 = pai2.getX2();
+
+        filhos.set(0,
+                new Individuo(x1Pai1, x2Pai2,
+                        this.funcao(x1Pai1, x2Pai2)));
+
+        filhos.set(1, new Individuo(x1Pai2, x2Pai1,
+                this.funcao(x1Pai2, x2Pai1)));
+
+        return filhos;
+    }
+
+    public Individuo mutacao(Individuo filho) {
+
+        return filho;
+    }
+
+}
---
+++ src/Individuo.java
@@ -0,0 +1,37 @@
+public class Individuo {
+
+    private double x1;
+    private double x2;
+    private double fit;
+
+    public Individuo(double x1, double x2, double fit) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.fit = fit;
+    }
+
+    public double getX1() {
+        return x1;
+    }
+
+    public double getX2() {
+        return x2;
+    }
+
+    public double getFit() {
+        return fit;
+    }
+
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
+
+    public void setX1(double x1) {
+        this.x1 = x1;
+    }
+
+    public void setFit(double fit) {
+        this.fit = fit;
+    }
+
+}
